83070c23d16eff406cc1267a29dcc74142861b2c
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-12-11 17:40:05 +0530

    remove  unawanted  data
    remove  unawanted  data

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -24,14 +24,6 @@
                     </ul>
                 </li>
 				
-                <li class="menu-item-has-children dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="menu-icon fa fa-users"></i>Sub Menu</a>
-                    <ul class="sub-menu children dropdown-menu">
-                        
-                        <li><i class="menu-icon fa fa-angle-right"></i><a href="datatable.php">Datatable</a></li>
-                    </ul>
-                </li>
-				
 				<li class="menu-item-has-children dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="menu-icon fa fa-users"></i>Category Creation</a>
                     <ul class="sub-menu children dropdown-menu">
