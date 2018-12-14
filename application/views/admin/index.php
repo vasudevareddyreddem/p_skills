f680e5aecc3ef92497b288254c1ee4466acbcf49
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-12-14 15:46:54 +0530

    added card boxes in admin index page

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,4 +1,3 @@
-
 <div class="breadcrumbs">
     <div class="col-sm-4">
         <div class="page-header float-left">
@@ -19,10 +18,77 @@
 </div>
 
 
+
 <div class="content mt-3">
     <div class="animated fadeIn">
         <div class="row">
-            
+
+            <div class="col-lg-3 col-md-3">
+                <div class="card">
+                    <div class="card-body bg-flat-color-4">
+                        <div class="h1 text-light text-right mb-4">
+                            <i class="fa fa-clock-o"></i>
+                        </div>
+                        <div class="h4 mb-0 text-light">5:34:11</div>
+                        <small class="text-light text-uppercase font-weight-bold">Avg. Time</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-light" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+            </div><!-- /# column -->
+
+            <div class="col-lg-3 col-md-3">
+                <div class="card">
+                    <div class="card-body bg-flat-color-5">
+                        <div class="h1 text-right text-light mb-4">
+                            <i class="fa fa-pie-chart"></i>
+                        </div>
+                        <div class="h4 mb-0 text-light">
+                            <span class="count">28</span>%
+                        </div>
+                        <small class="text-uppercase font-weight-bold text-light">Returning Visitors</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-light" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+            </div><!-- /# column -->
+
+            <div class="col-lg-3 col-md-3">
+                <div class="card">
+                    <div class="card-body bg-flat-color-3">
+                        <div class="h1 text-right mb-4">
+                            <i class="fa fa-cart-plus text-light"></i>
+                        </div>
+                        <div class="h4 mb-0 text-light">
+                            <span class="count">1238</span>
+                        </div>
+                        <small class="text-light text-uppercase font-weight-bold">Products sold</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-light" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+            </div><!-- /# column -->
+
+            <div class="col-lg-3 col-md-3">
+                <div class="card">
+                    <div class="card-body bg-flat-color-1">
+                        <div class="h1 text-light text-right mb-4">
+                            <i class="fa fa-comments-o"></i>
+                        </div>
+                        <div class="h4 mb-0 text-light">
+                            <span class="count">972</span>
+                        </div>
+                        <small class="text-light text-uppercase font-weight-bold">COMMENTS</small>
+                        <div class="progress progress-xs mt-3 mb-0 bg-light" style="width: 40%; height: 5px;"></div>
+                    </div>
+                </div>
+            </div><!-- /# column -->
+        </div>
+
+    </div><!-- .animated -->
+</div> <!-- .content -->
+
+<div class="content mt-3">
+    <div class="animated fadeIn">
+        <div class="row">
+
             <div class="col-lg-6">
                 <div class="card">
                     <div class="card-body">
@@ -62,5 +128,3 @@
 
     </div><!-- .animated -->
 </div> <!-- .content -->
-
-
