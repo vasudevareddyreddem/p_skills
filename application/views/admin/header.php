a9da45f20d964bfa99f0e0f8fc9b81f450546856
sivaksr <40159306+sivaksr@users.noreply.github.com>
2018-12-14 16:14:33 +0530

    CHANGE HEADER PRACHATECH REPLACE SKILL CHAIR
    CHANGE HEADER PRACHATECH REPLACE SKILL CHAIR

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Prachatech</title>
+    <title>Skill Chair</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/vendor/admin/img/fav.png">
